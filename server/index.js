365063cad1e38e321c0615b0a35d9e9a1b9737ea
NubianGeekess <nubiangeekess@gmail.com>
2019-06-06 00:58:40 +0200

    Basic config of our server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import './env';
+
+import express from 'express';
+
+// import models, { sequelize } from './models';
+
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server started on ${port}!!!`);
+});
